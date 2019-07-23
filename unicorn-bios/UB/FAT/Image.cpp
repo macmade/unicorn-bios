27d9c3f5f2daae2c7e6e9473ba5c7d841d3b02ac
macmade <macmade@xs-labs.com>
2019-07-23 23:03:17 +0200

    Preparing for FAT images...

---
+++ unicorn-bios/UB/FAT/Image.cpp
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2019 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#include "UB/FAT/Image.hpp"
+
+namespace UB
+{
+    namespace FAT
+    {
+        class Image::IMPL
+        {
+            public:
+                
+                IMPL( const std::string & path );
+                IMPL( const IMPL & o );
+                
+                std::string _path;
+        };
+        
+        Image::Image( const std::string & path ):
+            impl( std::make_unique< IMPL >( path ) )
+        {}
+        
+        Image::Image( const Image & o ):
+            impl( std::make_unique< IMPL >( *( o.impl ) ) )
+        {}
+        
+        Image::Image( Image && o ):
+            impl( std::move( o.impl ) )
+        {}
+        
+        Image::~Image( void )
+        {}
+        
+        Image & Image::operator =( Image o )
+        {
+            swap( *( this ), o );
+            
+            return *( this );
+        }
+        
+        std::string Image::path( void ) const
+        {
+            return this->impl->_path;
+        }
+        
+        void swap( Image & o1, Image & o2 )
+        {
+            using std::swap;
+            
+            swap( o1.impl, o2.impl );
+        }
+        
+        Image::IMPL::IMPL( const std::string & path ):
+            _path( path )
+        {}
+        
+        Image::IMPL::IMPL( const IMPL & o ):
+            _path( o._path )
+        {}
+    }
+}
---
+++ unicorn-bios/UB/FAT/Image.hpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2019 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#ifndef UB_FAT_IMAGE_HPP
+#define UB_FAT_IMAGE_HPP
+
+#include <memory>
+#include <algorithm>
+#include <string>
+
+namespace UB
+{
+    namespace FAT
+    {
+        class Image
+        {
+            public:
+                
+                Image( const std::string & path );
+                Image( const Image & o );
+                Image( Image && o );
+                ~Image( void );
+                
+                Image & operator =( Image o );
+                
+                std::string path( void ) const;
+                
+                friend void swap( Image & o1, Image & o2 );
+                
+            private:
+                
+                class IMPL;
+                std::unique_ptr< IMPL > impl;
+        };
+    }
+}
+
+#endif /* UB_FAT_IMAGE_HPP */
