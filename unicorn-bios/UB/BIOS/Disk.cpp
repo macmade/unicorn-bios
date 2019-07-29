aff3fb19532409aafaaadf4e7070f633baa20f3a
macmade <macmade@xs-labs.com>
2019-07-29 20:33:13 +0200

    Preparing to read sectors...

--- unicorn-bios/UB/BIOS/Disk.cpp
+++ unicorn-bios/UB/BIOS/Disk.cpp
@@ -26,6 +26,7 @@
 #include "UB/Machine.hpp"
 #include "UB/Engine.hpp"
 #include "UB/String.hpp"
+#include "UB/Casts.hpp"
 
 namespace UB
 {
@@ -43,8 +44,63 @@ namespace UB
             
             void readSectors( const Machine & machine, Engine & engine )
             {
-                ( void )machine;
-                ( void )engine;
+                uint8_t  driveNumber( engine.dl() );
+                uint8_t  sectors(     engine.al() );
+                uint8_t  cylinder(    engine.ch() );
+                uint8_t  sector(      engine.cl() );
+                uint8_t  head(        engine.dh() );
+                uint64_t address(     0 );
+                
+                if( driveNumber != 0x00 )
+                {
+                    machine.ui().debug() << "[ ERROR ]> Reading from drive " << String::toHex( driveNumber ) << " is not supported" << std::endl;
+                    
+                    goto error;
+                }
+                
+                address   = engine.es();
+                address <<= 4;
+                address  += engine.bx();
+                
+                machine.ui().debug() << "Reading " << static_cast< unsigned int >( sectors ) << " sector" << ( ( sectors > 1 ) ? "s" : "" ) << " from drive " << String::toHex( driveNumber )
+                                     << std::endl
+                                     << "  - Cylinder:    " << String::toHex( cylinder )
+                                     << std::endl
+                                     << "  - Head:        " << String::toHex( head )
+                                     << std::endl
+                                     << "  - Sector:      " << String::toHex( sector )
+                                     << std::endl
+                                     << "  - Destination: " << String::toHex( address ) << " (" << String::toHex( engine.es() ) << ":" << String::toHex( engine.bx() ) << ")"
+                                     << std::endl;
+                
+                {
+                    std::vector< uint8_t > bytes( machine.bootImage().read( sectors, cylinder, sector, head ) );
+                    
+                    if( bytes.size() == 0 )
+                    {
+                        machine.ui().debug() << "[ ERROR ]> No data received" << std::endl;
+                        
+                        goto error;
+                    }
+                    
+                    machine.ui().debug() << "[ SUCCESS ]> Writing " << bytes.size() << " to memory" << std::endl;
+                    
+                    memcpy( reinterpret_cast< void * >( address ), &( bytes[ 0 ] ), bytes.size() );
+                    
+                    {
+                        engine.cf( false );
+                        engine.ah( 0 );
+                        engine.al( sectors );
+                    }
+                }
+                
+                error:
+                    
+                    {
+                        engine.cf( true );
+                        engine.ah( 1 );
+                        engine.al( 0 );
+                    }
             }
         }
     }
