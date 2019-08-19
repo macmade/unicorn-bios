a1c2a45d0deaf809f38d5156c2ff73162b843584
macmade <macmade@xs-labs.com>
2019-08-19 19:32:31 +0200

    Description for more video modes.

--- unicorn-bios/UB/BIOS/Video.cpp
+++ unicorn-bios/UB/BIOS/Video.cpp
@@ -38,40 +38,59 @@ namespace UB
             bool setVideoMode( const Machine & machine, Engine & engine )
             {
                 uint8_t mode( engine.al() );
-
-                if ( machine.debugVideo() )
-                {
-                    machine.ui().debug() << "Setting video mode to: (" << String::toHex( mode ) << ")" << std::endl;
-
-                    switch ( mode )
-                    {
-                        case 0x03:
-                            machine.ui().debug() << "    - 80x25 16 color text (CGA, EGA, MCGA, VGA)" << std::endl;
-                            break;
-
-                        default:
-                            machine.ui().debug() << "    - Unknown mode " << std::endl;
-                    }
-                }
-
                 uint8_t maskedMode( mode & 0x7f );
-
-                if ( maskedMode > 7 )
+                
+                if( machine.debugVideo() )
+                {
+                    std::string description( "Unknown mode" );
+                    
+                    machine.ui().debug() << "Setting video mode to " << String::toHex( mode ) << ":" << std::endl;
+                    
+                    switch( mode )
+                    {
+                        case 0x00: description = "40x25 B/W text (CGA,EGA,MCGA,VGA)";                break;
+                        case 0x01: description = "40x25 16 color text (CGA,EGA,MCGA,VGA)";           break;
+                        case 0x02: description = "80x25 16 shades of gray text (CGA,EGA,MCGA,VGA)";  break;
+                        case 0x03: description = "80x25 16 color text (CGA,EGA,MCGA,VGA)";           break;
+                        case 0x04: description = "320x200 4 color graphics (CGA,EGA,MCGA,VGA)";      break;
+                        case 0x05: description = "320x200 4 color graphics (CGA,EGA,MCGA,VGA)";      break;
+                        case 0x06: description = "640x200 B/W graphics (CGA,EGA,MCGA,VGA)";          break;
+                        case 0x07: description = "80x25 Monochrome text (MDA,HERC,EGA,VGA)";         break;
+                        case 0x08: description = "160x200 16 color graphics (PCjr)";                 break;
+                        case 0x09: description = "320x200 16 color graphics (PCjr)";                 break;
+                        case 0x0A: description = "640x200 4 color graphics (PCjr)";                  break;
+                        case 0x0B: description = "Reserved (EGA BIOS function 11)";                  break;
+                        case 0x0C: description = "Reserved (EGA BIOS function 11)";                  break;
+                        case 0x0D: description = "320x200 16 color graphics (EGA,VGA)";              break;
+                        case 0x0E: description = "640x200 16 color graphics (EGA,VGA)";              break;
+                        case 0x0F: description = "640x350 Monochrome graphics (EGA,VGA)";            break;
+                        case 0x10: description = "640x350 16 color graphics (EGA or VGA with 128K)"; break;
+                        case 0x11: description = "640x480 B/W graphics (MCGA,VGA)";                  break;
+                        case 0x12: description = "640x480 16 color graphics (VGA)";                  break;
+                        case 0x13: description = "320x200 256 color graphics (MCGA,VGA)";            break;
+                        
+                        default: break;
+                    }
+                    
+                    machine.ui().debug() << "    - " << description << std::endl;
+                }
+                
+                if( maskedMode > 7 )
                 {
                     engine.al( 0x20 );
                 }
-                else if ( maskedMode == 6 )
+                else if( maskedMode == 6 )
                 {
-                    engine.al( 0x3f );
+                    engine.al( 0x3F );
                 }
                 else
                 {
                     engine.al( 0x30 );
                 }
-
+                
                 return true;
             }
-
+            
             bool setCursorPosition( const Machine & machine, Engine & engine )
             {
                 if( machine.debugVideo() )
